9551f6cb5d1b1d6162c0985f92b62bf1ec390311
Ricardo Tierra <ricardo.anthony.tierra@Gmail.com>
2019-11-10 21:01:00 -0500

    first commit

---
+++ src/database.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose.set('useFindAndModify', false);
+mongoose.connect('mongodb+srv://ricardo:basededatosdericardo@sky-database-riqjg.gcp.mongodb.net/GARAGE?retryWrites=true&w=majority', {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true 
+})
+  .then(db => console.log('DB is connected'))
+  .catch(err => console.error(err));
